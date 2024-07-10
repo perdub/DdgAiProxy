111a6585c6ae51127c588a44380d66c6959235ab
perdub <my.emailtounity@gmail.com>
2024-07-10 12:08:09 +0300

    add base example

---
+++ examples/Base/Program.cs
@@ -0,0 +1,5 @@
+﻿using DdgAiProxy;
+DialogManager dialogManager = new DialogManager(new CustomClient());
+await dialogManager.Init(Model.Gpt3_5_turbo);
+string response = await dialogManager.SendMessage("Hello! How are you? And who are you?");
+Console.WriteLine(response);
